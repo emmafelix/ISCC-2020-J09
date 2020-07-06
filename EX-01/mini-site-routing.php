fbcf514164b5c0a2ff79ca42a296dcc556e6df97
emmafelix <emma.felix@iseg.fr>
2020-07-06 12:34:00 +0200

    EX-01-Connexion et Sessions

---
+++ EX-01/mini-site-routing.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+<title>mini-site-routing</title>
+</head>
+<body>
+    <header>
+<nav>
+    <a href="http://localhost:8888/ISSC%20-%202020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=1">Accueil</a>
+    <a href="http://localhost:8888/ISSC%20-%202020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=2">Page1</a>
+    <a href="http://localhost:8888/ISSC%20-%202020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=3">Page2</a>
+    <a href="http://localhost:8888/ISSC%20-%202020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=connexion">Connexion</a>
+</nav>
+    </header>
+    <h1>
+        <?php
+
+        session_start();
+        if($_GET['page']==1){
+            echo "Accueil !";
+            include ('Accueil.php');
+            
+        }
+        elseif($_GET['page']==2){
+            echo "Page 2 !";
+            include ('Page1.php');   }
+
+        elseif($_GET['page']=='connexion'){
+            echo "Connexion !";
+            include ('Connexion.php') ;
+        }
+        elseif($_GET['page']==3){
+            echo "Page 3 !";
+            include ('Page2.php');
+        }
+        ?>
+    </h1>
+    <p>
+<?php
+if(array_key_exists('id',$_SESSION)==true){
+echo 'Login: '.$_SESSION['login'].'.';}
+?>
+</p>
+</body>
+</html>
---
+++ EX-01/securite.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+echo $_POST['login'];
+echo '<br>';
+echo $_POST['password'];
+echo '<br>';
+
+if ($_POST['password']=='2020'){
+    $_SESSION['id']=$_POST['login'];
+    
+header('Location:http://localhost:8888/ISSC%20-%202020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=1');
+
+}
+elseif($_POST['password'] !='2020'){
+    echo 'Mauvais couple identifiant / mot de passe.<br>';
+    echo '<a href="http://localhost:8888/ISSC%20-%202020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=connexion">Connexion</a>';
+}
+?>
